e3b1b8db1b8b0e81fed4c9e995e90c510f7ca105
Joshua Wong <dsjw2@cam.ac.uk>
2017-02-26 16:41:47 +0000

    Update TradingMain.cc

--- TradingMain.cc
+++ TradingMain.cc
@@ -27,18 +27,18 @@ int main() {
    CalStats();
 	
    for (int dataset=1; dataset<21; dataset++) {
-		// Call to function to initialise trading account
+		// Initialise trading account
 		InitialiseAccount(dataset, 74, 2+333+15, account, dataset); 
 
-		// Call to function to trade and update accounts over next 50 days
+		// Trade and update accounts over next 50 days
 		MyTrader(account, data, dataset);
 		
-		//Call to function to close trading account and output accounts and add sum to total
+		//Close trading account and output accounts and add sum to total
 		account.compiledtotal += ConcludeAcc(account, dataset);
 
 	}
    
-	// Set up graphics. 
+	// Set up GUI
    glueWindow(1500,900,50,50,WHITE); 
    graphicsfunction(mygraphics); 
    glueGo();
